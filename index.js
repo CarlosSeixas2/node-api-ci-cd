606d978c2cb281a6c3bfbc1aba485fa431a533da
CarlosSeixas2 <carlosseixasof@gmail.com>
2025-02-27 16:36:00 -0300

    adicionando rota de subtração

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.get("/", (req, res) => {
   res.json({ message: "API funcionando!" });
 });
 
+app.get("subtracao", (req, res) => {
+  const { a, b } = req.query;
+  if (typeof a !== "number" || typeof b !== "number") {
+    return res.status(400).json({ error: "Os valores devem ser números" });
+  }
+  res.json({ resultado: a - b });
+});
+
 app.post("/soma", (req, res) => {
   const { a, b } = req.body;
   if (typeof a !== "number" || typeof b !== "number") {
